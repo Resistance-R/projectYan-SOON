4c132032044b980b00d55dc753fd8f06ca966b9a
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2024-01-24 19:11:31 +0900

    model design

--- projectYan-SOON/main/Model.py
+++ projectYan-SOON/main/Model.py
@@ -1,5 +1,86 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-"""
-모델 설계 코드
-"""
+def build_transformer_model(max_len, vocab_size, embedding_dim=256, num_heads=8, ff_dim=4):
+    inputs = layers.Input(shape=(max_len,))
+    
+    embedding_layer = layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim)(inputs)
+    transformer_block = TransformerBlock(embedding_dim, num_heads, ff_dim)(embedding_layer)
+    transformer_block = TransformerBlock(embedding_dim, num_heads, ff_dim)(transformer_block)
+    transformer_block = TransformerBlock(embedding_dim, num_heads, ff_dim)(transformer_block)
+    
+    pooled_output = layers.GlobalAveragePooling1D()(transformer_block)
+    outputs = layers.Dense(1, activation='sigmoid')(pooled_output)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+class MultiHeadSelfAttention(layers.Layer):
+    def __init__(self, embed_dim, num_heads=8):
+        super(MultiHeadSelfAttention, self).__init__()
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        if embed_dim % num_heads != 0:
+            raise ValueError(f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}")
+        self.projection_dim = embed_dim // num_heads
+        self.query_dense = layers.Dense(embed_dim)
+        self.key_dense = layers.Dense(embed_dim)
+        self.value_dense = layers.Dense(embed_dim)
+        self.combine_heads = layers.Dense(embed_dim)
+
+    def attention(self, query, key, value):
+        score = tf.matmul(query, key, transpose_b=True)
+        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
+        scaled_score = score / tf.math.sqrt(dim_key)
+        weights = tf.nn.softmax(scaled_score, axis=-1)
+        output = tf.matmul(weights, value)
+        return output, weights
+
+    def separate_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        query = self.query_dense(inputs)
+        key = self.key_dense(inputs)
+        value = self.value_dense(inputs)
+        query = self.separate_heads(query, batch_size)
+        key = self.separate_heads(key, batch_size)
+        value = self.separate_heads(value, batch_size)
+        attention, weights = self.attention(query, key, value)
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+        concat_attention = tf.reshape(attention, (batch_size, -1, self.embed_dim))
+        output = self.combine_heads(concat_attention)
+        return output
+
+class TransformerBlock(layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
+        self.ffn = keras.Sequential([
+            layers.Dense(ff_dim, activation="relu"),
+            layers.Dense(embed_dim),
+        ])
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs):
+        attn_output = self.att(inputs)
+        attn_output = self.dropout1(attn_output)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output)
+        return self.layernorm2(out1 + ffn_output)
+
+# 모델 생성
+max_len = 100  # 문장의 최대 길이
+vocab_size = 10000  # 단어 집합의 크기
+model = build_transformer_model(max_len, vocab_size)
+
+# 모델 요약 보기
+model.summary()
